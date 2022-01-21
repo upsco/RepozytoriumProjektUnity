f6c469f32d6f1a54c70b16995e3f732e7a01abe1
upsco <80310080+upsco@users.noreply.github.com>
2022-01-21 20:44:56 +0100

    Kolejne zmiany

--- TextGameUnity/Assets/Scripts/TextGame.cs
+++ TextGameUnity/Assets/Scripts/TextGame.cs
@@ -1,18 +1,60 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TextGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    TextGame textUI;
+
+    void AudioLoader()
     {
-        
+        GameObject gameObject = GameObject.Find("Canvas");
+        AudioSource audio = gameObject.GetComponent<AudioSource>();
+        audio.Play();
     }
 
-    // Update is called once per frame
+    int SceneChecker()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "MainMenu")
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    void TextLoader ()
+    {
+        Text textUI;
+        textUI = GetComponent<Text>();
+
+        if (SceneChecker() == 0)
+        {
+            textUI.text =    "Znajdujesz się w ciemnym domu \n" +
+                             "Nie ma w nim żadnego światła \n" +
+                             "A ty szukasz wyjścia z tej ciemności, dlatego dotknij ENTER \n";
+        }
+        else
+        {
+            textUI.text = "Opis lokalizacji";
+        }
+    }
+
+    void Start ()
+    {
+        SceneChecker();
+        TextLoader();
+        AudioLoader();
+    }
+    
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Return))
+            SceneManager.LoadScene("Levels");
     }
 }
